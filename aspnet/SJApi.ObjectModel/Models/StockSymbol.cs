501345eb5085cd2eee299e85db1eb7883ae7913c
Daniel Hopson <hdhopson88@yahoo.com>
2021-04-23 19:07:04 -0700

    Creates stock symbol model

--- aspnet/SJApi.ObjectModel/Models/StockSymbol.cs
+++ aspnet/SJApi.ObjectModel/Models/StockSymbol.cs
@@ -1,10 +1,22 @@
-namespace SJAPI.ObjectModel.Models
+using System;
+
+namespace SJApi.ObjectModel.Models
 {
     public class StockSymbol
     {
         public string Symbol { get; set; }
+        public string exchange { get; set; }
+        public string ExchangeSuffix { get; set; }
         public string Name { get; set; }
         public string ExchangeName { get; set; }
         public string Currency { get; set; }
+        public DateTime date { get; set; }
+        public string type { get; set; }        
+        public string iexId { get; set; }
+        public string region { get; set; }        
+        public bool isEnabled { get; set; }
+        public string figi { get; set; }
+        public string cik { get; set; }
+        public string lei { get; set; }
     }
 }
